6051900f06be692e65a338cf52976223ecfd76cf
Akhee123 <mabuirmelah@gmail.com>
2023-04-13 02:55:02 +0300

    login signup tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import LoginSignupTabs from './components/LoginSignupTabs/LoginSignupTabs';
 import MusicPlayer from './components/MusicPlayer/MusicPlayer';
 import NavBar from './components/NavBar/NavBar';
 import TopChartsList from './components/TopChartsList/TopChartsList';
@@ -5,9 +6,7 @@ import TopChartsList from './components/TopChartsList/TopChartsList';
 export default function App() {
 	return (
 		<>
-			<NavBar />
-			<TopChartsList />
-			<MusicPlayer />
+			<LoginSignupTabs />
 		</>
 	);
 }
